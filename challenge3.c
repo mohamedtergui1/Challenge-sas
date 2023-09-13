39b4973a318ba2618beedd5c095664571f1b3e1b
mohamedtergui1 <144792669+mohamedtergui1@users.noreply.github.com>
2023-09-13 21:49:59 +0200

    Update challenge3.c

--- challenge3.c
+++ challenge3.c
@@ -7,11 +7,11 @@ int main()
     float resultat;
     char c;
     printf("entre oppiration tu peux te calculer:");scanf("%d%c%d", &a, &c, &b);
-    if (c='+')resultat=a+b;
-    else if (c='-')resultat=a-b;
-    else if (c='*')resultat=a*b;
-    else if (c='/')resultat=a/b;
-    else if (c='%')resultat=a%b;
+    if (c=='+') { resultat=a+b;}
+     else if (c=='-'){resultat=a-b;}
+    else if (c=='*'){resultat=a*b;}
+    else if (c=='/'){resultat=(float)a/b;}
+    else if (c=='%'){resultat=a%b;}
 
     printf("le resultat est : %.2f ",resultat);
     return 0;
