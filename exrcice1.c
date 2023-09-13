91c77d068242da905a62e8453df684a0d1733a3e
mohamed <simotergui4@gmail.com>
2023-09-13 14:07:08 +0200

    exercice 1

---
+++ exrcice1.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main() {
+    char nom[20],prenom[20],sexe[10];
+    int age ;
+    int numero;
+
+            printf("entrer votr nome :");scanf("%s",nom);
+            printf("entrer votr prenom :");scanf("%s",prenom);
+            printf("entrer votr age :");scanf("%d",&age);
+            printf("entrer votr sex (f:m) :"); scanf("%s",&sexe);
+            printf("entrer votr numero de telephone :");scanf("%d",&numero);
+
+
+            printf("\n *  nome :%s",nom);
+            printf("\n *  prenom :%s",prenom);
+            printf("\n *  age :%d",age);
+            printf("\n *  sexe :%s",sexe);
+            printf("\n *  numero de telephone :0%ld",numero);
+
+    return 0;
+}
