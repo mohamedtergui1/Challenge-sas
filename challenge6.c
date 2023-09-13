4c3503adafb3917bc456d126239f520b13e02e30
mohamed <simotergui4@gmail.com>
2023-09-13 18:08:20 +0200

    ex6

---
+++ challenge6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    float r , circonference ;
+    printf("entrer la valeur de rayon:");scanf("%f",&r);
+    circonference = 2*r*3.14;
+    printf("entrer la valeur de rayon:%.2f",circonference);
+}
