57ca672c7d0f814d0a1688a5a081ffd6f1cd5894
mohamed <simotergui4@gmail.com>
2023-09-14 12:13:14 +0200

    challenge 3 => condition

---
+++ sas_challengers_conditions/chqllenge3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+       int a,b , tripl, somme;
+       printf("entrer le nomber 1:");scanf("%d",&a);
+       printf("entrer le nomber 2:");scanf("%d",&b);
+       somme=a+b;
+       tripl=3*somme;
+       printf("la somme est :%d",somme);
+       printf("\nle triple est :%d",tripl);
+    return 0;
+}
