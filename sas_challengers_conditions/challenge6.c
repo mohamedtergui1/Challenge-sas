57864bcc55118bc2460ac0f3cb233aaf4f74ddf3
mohamed <simotergui4@gmail.com>
2023-09-14 13:18:00 +0200

    challenge6 => condition

---
+++ sas_challengers_conditions/challenge6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      int a;
+      printf("entrer le nomber :");scanf("%d",&a);
+      if(a<0) printf("le nomber est negative ");
+      else if (a==0) printf("le nomber  egal zero ");
+      else printf("le nomber est positive");
+
+
+    return 0;
+}
