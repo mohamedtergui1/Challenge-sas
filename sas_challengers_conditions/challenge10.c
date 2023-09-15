21af1e108bf90eb309cd5b9ac771d2c9b96f8367
mohamedtergui1 <144792669+mohamedtergui1@users.noreply.github.com>
2023-09-15 10:48:10 +0200

    Update challenge10.c

--- sas_challengers_conditions/challenge10.c
+++ sas_challengers_conditions/challenge10.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int aa,mm,jj;
-        printf("donnee la date ce formme 15/09/2012:");scanf("%d/%d/%d",&jj,&mm,&aa);
-         if (jj<10&&mm<10)printf("0%d-0%d-%d",jj,mm,aa);
-         else if (jj<10) printf("%d-0%d-%d",jj,mm,aa);
-         else if (mm<10) printf("0%d-%d-%d",jj,mm,aa);
-         else  printf("%d-%d-%d",jj,mm,aa);
+int main(void) {
+    int jour, mois, annee;
+
+    printf("Entrer la date (jour/mois/annee): ");
+    scanf("%d/%d/%d", &jour, &mois, &annee);
+
+    printf("%d-", jour);
+
+    switch (mois) {
+        case 1: printf("Janvier"); break;
+        case 2: printf("Fevrier"); break;
+        case 3: printf("Mars"); break;
+        case 4: printf("Avril"); break;
+        case 5: printf("Mai"); break;
+        case 6: printf("Juin"); break;
+        case 7: printf("Juillet"); break;
+        case 8: printf("Aout"); break;
+        case 9: printf("Septembre"); break;
+        case 10: printf("Octobre"); break;
+        case 11: printf("Novembre"); break;
+        case 12: printf("Decembre"); break;
+        default: printf("Error"); break;
+    }
+
+    printf("-%d\n", annee);
+
     return 0;
 }
-
-
-
