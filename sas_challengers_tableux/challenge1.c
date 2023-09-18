649dbb8c1ef053dcbb8131ba8a89d08233bc932d
mohamed <simotergui4@gmail.com>
2023-09-18 10:40:27 +0200

    callenge 1 => tableux

---
+++ sas_challengers_tableux/challenge1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int tab[10]={15,748,5,15,6541,651,5,154,56,65};
+    for (int i=0;i<10;i++){
+        printf("element %d:%d\n",i,tab[i]);
+    }
+    return 0;
+}
